661dfd98030d7c74637e8ef2405264573819f72f
Roland Clairquin <rolandcl@azrael.fontaine>
2018-02-13 21:00:28 +0100

    Creation du projet avec 'lein new compojure nomad-web-clj Affichage du contenu d'un NomadStorageCache (sans formatage)

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject nomad-web-clj "0.1.0-SNAPSHOT"
+  :description "A web interface to NOMAD HDF5 data"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.1"]
+                 [ring "1.6.3"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.xerial/sqlite-jdbc "3.21.0.1"]
+                 [honeysql "0.9.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler nomad-web-clj.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ src/nomad_web_clj/handler.clj
@@ -0,0 +1,40 @@
+(ns nomad-web-clj.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.handler.dump :refer [handle-dump]]
+            )
+  (:require [clojure.java.io :as io]
+            [clojure.java.jdbc :as jdbc]
+            [honeysql.core :as sql]
+            ))
+
+;; NOMAD Storage --------------------------------------------------------------
+(def NOMAD-DATA-DIR "/home/rolandcl/Projects/NOMAD/data")
+(def NOMAD-HDF5-DIR (io/file NOMAD-DATA-DIR "hdf5"))
+
+(defn read-cache [hdf5_lev]
+  (let [cache-file (io/file NOMAD-HDF5-DIR hdf5_lev "cache.db")
+        cache-db {:dbtype "sqlite"
+                  :dbname cache-file}
+        ]
+    (->> (sql/format {:select [:*]
+                      :from   [:files]
+                      :order-by [:beg_dtime]})
+         (jdbc/query cache-db))
+    ))
+
+;;-----------------------------------------------------------------------------
+
+(defn list-files [request]
+  (-> (:uri request)
+      (subs 1)
+      read-cache))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/hdf5_level_0p1c" [] list-files)
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
